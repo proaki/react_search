515136988fc689d9445c534b5bd3c9197ea12d0e
Akihiro Inui <mail@akihiroinui.com>
2021-07-16 10:53:05 +0200

    Changed elastic host

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,60 @@
-import React, { Component } from "react";
-import "./App.css";
-import PersonList from "./components/PersonList";
+import React, {Component} from 'react';
+import {DataSearch, ReactiveBase, ReactiveList, ResultList, SelectedFilters} from '@appbaseio/reactivesearch';
+import './App.css';
+
+const { ResultListWrapper } = ReactiveList;
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <div className="container-fluid">
-          <PersonList />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="main-container">
+                <ReactiveBase
+                    app="steam-search"
+                    url="http://52.142.54.111:9200"
+                >
+                    <DataSearch
+                        componentId="title"
+                        dataField={["ResponseName"]}
+                        queryFormat="and"
+                    />
+                    <SelectedFilters/>
+                    <ReactiveList
+                        componentId="resultLists"
+                        dataField="ResponseName"
+                        size={10}
+                        pagination={true}
+                        react={{
+                            "and": ["title"]
+                        }}
+                    >
+                        {({data}) => (
+                            <ResultListWrapper>
+                                {
+                                    data.map(item => (
+                                        <ResultList key={item._id}
+                                                    href={`https://store.steampowered.com/app/${item.ResponseID}`}>
+                                            <ResultList.Image src={item.HeaderImage}/>
+                                            <ResultList.Content>
+                                                <ResultList.Title
+                                                    dangerouslySetInnerHTML={{
+                                                        __html: item.ResponseName
+                                                    }}
+                                                />
+                                                <ResultList.Description>
+                                                  <p className="releaseDate">${item.ReleaseDate}</p>
+                                                  <p className="price">$${item.PriceInitial}</p>
+                                                </ResultList.Description>
+                                            </ResultList.Content>
+                                        </ResultList>
+                                    ))
+                                }
+                            </ResultListWrapper>
+                        )}
+                    </ReactiveList>
+                </ReactiveBase>
+            </div>
+        );
+    }
 }
 
 export default App;
